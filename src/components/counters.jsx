d524725b2c66c86ce1ece144877c87cffb9f0f55
bharat_thapa <admin@bharatt.com.np>
2021-02-17 09:29:20 +0545

    styles changed

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -7,10 +7,10 @@ class Counters extends Component {
         const { onReset, counters, onIncrement, onDelete, onDecrement } = this.props;
         return ( 
             <div>
-                <button onClick={ onReset } className="btn btn-primary m-2">Reset</button>
                 { counters.map( counter => (
                     <Counter onDecrement={onDecrement} onIncrement={onIncrement} onDelete={onDelete} key={counter.id} counter={counter} />
-                )) }
+                    )) }
+                <button onClick={ onReset } className="reset-btn btn btn-primary m-2">Reset</button>
             </div> );
     }
 }
